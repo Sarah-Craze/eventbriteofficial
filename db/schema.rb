15569578b7b6615f275fbb7ba49c76eccf5be2f7
Sarah-Craze <sarahcraze13@gmail.com>
2023-11-13 18:53:27 +0100

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_155424) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
